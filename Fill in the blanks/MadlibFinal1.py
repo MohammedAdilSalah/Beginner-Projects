e79a9eb27bf0427858f420a214a4c835aee1d6a8
Mohammed Adil Salah <108335418+MohammedAdilSalah@users.noreply.github.com>
2022-06-30 14:54:44 +0530

    Add files via upload

---
+++ Fill in the blanks/MadlibFinal1.py
@@ -0,0 +1,4 @@
+import FavCarMD, SuperHeroMD, FavDestiMD
+import random
+m = random.choice([FavCarMD, SuperHeroMD, FavDestiMD])
+m.madlib()
---
+++ Fill in the blanks/SuperHeroMD.py
@@ -0,0 +1,11 @@
+def madlib():
+ print ("------I am Superhero------")
+ w1 = input("What is your made up SuperHero name: ")
+ w2 = input("Name Your power: ")
+ w3 = input("Who will you protect: ")
+ w4 = input("Make a tagline: ")
+ madlib_final = f"My name is {w1}. I am the greatest superhero the world has ever known.\
+My power is {w2}. So dont mess with me. I will protect {w3} with my {w2}. Dont be afraid cause {w4}!!"
+ print(madlib_final)
+ 
+madlib()
